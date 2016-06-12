f25c30cbe1b454d8dcd73a0404233dbdaa0cd866
Gaponec <uaGaponec@gmail.com>
2016-06-12 18:39:29 +0300

    socket server and app

---
+++ app/src/main/java/com/example/gaponec/devicecontrolapp/other/WsConfig.java
@@ -0,0 +1,8 @@
+package com.example.gaponec.devicecontrolapp.other;
+
+/**
+ * Created by gaponec on 6/12/16.
+ */
+public class WsConfig {
+    public static final String URL_WEBSOCKET = "ws://10.0.2.2:8080/ChatServer/chat?name=";
+}
